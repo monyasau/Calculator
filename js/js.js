c481476e394c15e1693e93fe19c1aa12decee84d
Olajide olanrewaju <Olajide.m.olanrewaju@gmail.com>
2021-02-21 14:47:22 +0100

    js:
    added some js action
    and a preloader.

---
+++ js/js.js
@@ -0,0 +1,8 @@
+
+ //preloader
+      var preloader = document.getElementById("loading");
+      function myfunction() {
+        //this one would go and tell the browser not to show the preloader
+        preloader.style.display = "none";
+      }
+      //preloader ends
