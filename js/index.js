db342b7bb87cc988b240aa182947c0e8745af410
Olajide olanrewaju <Olajide.m.olanrewaju@gmail.com>
2021-05-26 18:38:29 +0100

    Finalee:
    I think this might be the last commit................

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
-btn1 = document.getElementById(button1);
-btn2 = document.getElementById(button2);
-btn3 = document.getElementById(button3);
-btn4 = document.getElementById(button4);
-btn5 = document.getElementById(button5);
-btn6 = document.getElementById(button6);
-btn7 = document.getElementById(button7);
-btn8 = document.getElementById(button8);
-btn9 = document.getElementById(button9);
-btnc = document.getElementById(buttonc);
-btnx = document.getElementById(buttonx);
-btndiv = document.getElementById(buttondiv);
-btnc.onclick = cleared();
+// btn1 = document.getElementById(button1);
+// btn2 = document.getElementById(button2);
+// btn3 = document.getElementById(button3);
+// btn4 = document.getElementById(button4);
+// btn5 = document.getElementById(button5);
+// btn6 = document.getElementById(button6);
+// btn7 = document.getElementById(button7);
+// btn8 = document.getElementById(button8);
+// btn9 = document.getElementById(button9);
+// btnc = document.getElementById(buttonc);
+// btnx = document.getElementById(buttonx);
+// btndiv = document.getElementById(buttondiv);
+// btnc.onclick = cleared();
 // btn1 = document.getElementById(button1);
 // btn1 = document.getElementById(button1);
 // btn1 = document.getElementById(button1);
@@ -32,35 +32,57 @@ btnc.onclick = cleared();
       //   }
       // }
       // Register service worker to control making site work offline
+// if ("serviceWorker" in navigator) {
+//   navigator.serviceWorker.register("sw.js").then(() => {
+//     console.log("Service Worker Registered");
+//   });
+// }
+// // Code to handle install prompt on desktop
+// let deferredPrompt;
+// const addBtn = document.querySelector(".add-button");
+// addBtn.style.display = "none";
+// window.addEventListener("beforeinstallprompt", (e) => {
+//   // Prevent Chrome 67 and earlier from automatically showing the prompt
+//   e.preventDefault();
+//   // Stash the event so it can be triggered later.
+//   deferredPrompt = e;
+//   // Update UI to notify the user they can add to home screen
+//   addBtn.style.display = "block";
+//   addBtn.addEventListener("click", () => {
+//     // hide our user interface that shows our A2HS button
+//     addBtn.style.display = "none";
+//     // Show the prompt
+//     deferredPrompt.prompt();
+//     // Wait for the user to respond to the prompt
+//     deferredPrompt.userChoice.then((choiceResult) => {
+//       if (choiceResult.outcome === "accepted") {
+//         console.log("User accepted the A2HS prompt");
+//       } else {
+//         console.log("User dismissed the A2HS prompt");
+//       }
+//       deferredPrompt = null;
+//     });
+//   });
+// });
+
+
+
+      document.cookie = "username=user001; expires=sat, 18 Dec 2021 12:00:00 UTC; path=/";
+
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("sw.js").then(() => {
-    console.log("Service Worker Registered");
-  });
-}
-// Code to handle install prompt on desktop
-let deferredPrompt;
-const addBtn = document.querySelector(".add-button");
-addBtn.style.display = "none";
-window.addEventListener("beforeinstallprompt", (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI to notify the user they can add to home screen
-  addBtn.style.display = "block";
-  addBtn.addEventListener("click", () => {
-    // hide our user interface that shows our A2HS button
-    addBtn.style.display = "none";
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === "accepted") {
-        console.log("User accepted the A2HS prompt");
-      } else {
-        console.log("User dismissed the A2HS prompt");
+  window.addEventListener("load", function () {
+    navigator.serviceWorker.register("sw.js", { scope: "/" }).then(
+      function (registration) {
+        // Registration was successful
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      },
+      function (err) {
+        // registration failed :(
+        console.log("ServiceWorker registration failed: ", err);
       }
-      deferredPrompt = null;
-    });
+    );
   });
-});
+}  
