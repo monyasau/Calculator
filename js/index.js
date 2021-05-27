839200376b1a2a7cc8c29b41dd11a416e66bae93
Olajide olanrewaju <Olajide.m.olanrewaju@gmail.com>
2021-05-27 10:14:47 +0100

    fixes: added some contacts

--- js/index.js
+++ js/index.js
@@ -1,42 +1,4 @@
-// btn1 = document.getElementById(button1);
-// btn2 = document.getElementById(button2);
-// btn3 = document.getElementById(button3);
-// btn4 = document.getElementById(button4);
-// btn5 = document.getElementById(button5);
-// btn6 = document.getElementById(button6);
-// btn7 = document.getElementById(button7);
-// btn8 = document.getElementById(button8);
-// btn9 = document.getElementById(button9);
-// btnc = document.getElementById(buttonc);
-// btnx = document.getElementById(buttonx);
-// btndiv = document.getElementById(buttondiv);
-// btnc.onclick = cleared();
-// btn1 = document.getElementById(button1);
-// btn1 = document.getElementById(button1);
-// btn1 = document.getElementById(button1);
-// btn1 = document.getElementById(button1);
-// btn1 = document.getElementById(button1);
-// btn1 = document.getElementById(button1);
-// btn1 = document.getElementById(button1);
-      // window.onscroll = function () {
-      //   resizeHeader();
-      // };
-      // function resizeHeader() {
-      //   if (
-      //     document.body.scrollTop > 80 ||
-      //     document.documentElement.scrollTop > 80
-      //   ) {
-      //     document.querySelector(".main-header").classList.add("resized");
-      //   } else {
-      //     document.querySelector(".main-header").classList.remove("resized");
-      //   }
-      // }
-      // Register service worker to control making site work offline
-// if ("serviceWorker" in navigator) {
-//   navigator.serviceWorker.register("sw.js").then(() => {
-//     console.log("Service Worker Registered");
-//   });
-// }
+
 // // Code to handle install prompt on desktop
 // let deferredPrompt;
 // const addBtn = document.querySelector(".add-button");
